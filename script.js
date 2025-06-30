7ead7c9a1e6d263be26312137db509493af498f6
nikosathanasopoulos <nikosathanasopoulos@users.noreply.github.com>
2025-06-30 13:46:25 +0000

    deploy: 91f183fd3b1220799132f3c68d2a918bdadfcfa8

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ const cocktailImg = document.querySelector('.cocktail__img');
 
 
 const btnInput = document.querySelector('.input__btn');
+const btnInputName = document.querySelector('.input__name__btn');
 
 const link = document.getElementById('cocktail-link');
 const result = document.getElementById('result');
+const homepage = document.getElementById('homepage');
 
 
-
-const renderCountry = function(data, className = ''){
+const renderCocktail = function(data, className = ''){
     const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${data.idDrink}`;
     const html = `
            <article class="cocktail ${className}">
@@ -32,7 +33,6 @@ const renderCountry = function(data, className = ''){
     cocktailsContainer.insertAdjacentHTML('beforeend', html);
 }
 
-
 const getJSON = function(url, errorMsg = 'Something went wrong.') {
     return fetch(url).then(response => {
         if(!response.ok){
@@ -42,14 +42,44 @@ const getJSON = function(url, errorMsg = 'Something went wrong.') {
     })
 };
 
+const getRandomCocktail = async function(){
 
-// New syntax with async await
+    try{
+        let data =  await getJSON(`https://thecocktaildb.com/api/json/v1/1/random.php`, 'Cocktail not found')
+        console.log(data);
+
+        const drink = data.drinks[0];
+        console.log(drink);
+
+        // Render the data
+        homepage.innerHTML = `
+            <h2>${drink.strDrink}</h2>
+            <img src="${drink.strDrinkThumb}" width="200" />
+            <p>${drink.strInstructions}</p>
+          `;
+
+        for (let i = 1; i <= 15; i++) {
+            const ing = drink[`strIngredient${i}`];
+            const meas = drink[`strMeasure${i}`];
+            if(ing){
+                homepage.innerHTML += `
+                <p>${ing} -- ${meas} </p>
+                `
+            }
+        }
+    }catch(err){alert(err)}
+};
+
+if (window.location.pathname.endsWith('index.html') || window.location.pathname === '/' || window.location.pathname === '/js_cocktails_api/') {
+    getRandomCocktail();
+}
+
 const getCocktailFromIngredient = async function(ingredient){
 
     try{
         let data =  await getJSON(`https://thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`, 'Country not found')
         console.log(data.drinks);
-        data.drinks.forEach(drink => {renderCountry(drink);})
+        data.drinks.forEach(drink => {renderCocktail(drink);})
 
 
         cocktailsContainer.style.opacity = 1;
@@ -57,11 +87,33 @@ const getCocktailFromIngredient = async function(ingredient){
 
 };
 
-btnInput.addEventListener('click', function(e){
-    e.preventDefault();
-    cocktailsContainer.innerHTML = '';
-    getCocktailFromIngredient(cocktailInput.value.toLowerCase());
-});
+const getCocktailFromName = async function(name){
+    console.log('Fetching cocktail:', name);
+    try{
+        let data =  await getJSON(`https://thecocktaildb.com/api/json/v1/1/search.php?s=${name}`, 'Country not found')
+        console.log(data.drinks);
+        data.drinks.forEach(drink => {renderCocktail(drink);})
+
+        cocktailsContainer.style.opacity = 1;
+    }catch(err){alert(err)}
+
+};
+
+if (btnInput) {
+    btnInput.addEventListener('click', function(e){
+        e.preventDefault();
+        cocktailsContainer.innerHTML = '';
+        getCocktailFromIngredient(cocktailInput.value.toLowerCase());
+    });
+}
+
+if (btnInputName) {
+    btnInputName.addEventListener('click', function(e){
+        e.preventDefault();
+        cocktailsContainer.innerHTML = '';
+        getCocktailFromName(cocktailInput.value.toLowerCase());
+    });
+}
 
 
 cocktailsContainer.addEventListener('click', function(e) {
@@ -83,7 +135,7 @@ cocktailsContainer.addEventListener('click', function(e) {
 const getCocktail = async function(id){
 
     try{
-        let data =  await getJSON(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`, 'Country not found')
+        let data =  await getJSON(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`, 'Cocktail not found')
         console.log(data);
 
         const drink = data.drinks[0];
@@ -106,10 +158,15 @@ const getCocktail = async function(id){
             }
         }
 
-
+        // ✅ Scroll to top after selecting
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
 
     }catch(err){alert(err)}
-
 };
+
+
 
 
